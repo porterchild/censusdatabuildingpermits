9b2de9e63119f80bb0681b7d87dde33aa539b42f
Porter Child <pchild28@gmail.com>
2017-11-17 15:14:39 -0700

    new method, just throw them all in one file, by county and msa, Excel makes sense of it

---
+++ aggregate_all_county.py
@@ -0,0 +1,15 @@
+import urllib2
+import time
+months = ['01','02','03','04','05','06','07','08','09','10','11','12']
+#months = ['01']
+year = '16'
+url_beginning = "https://www2.census.gov/econ/bps/County/co"
+url_end = "c.txt"
+for month in months:
+        response = urllib2.urlopen(url_beginning + year + month + url_end)
+        html = response.read()
+        html = html.splitlines()
+        for line in html:
+                print line
+        time.sleep(5)#don't overload server
+
